4901c29f61cf021e211d253575f82867be9cacd0
Hailey.forar <Hailey.forar@MU-C1-20.mullen.pvt.k12.co.us>
2019-09-10 11:55:01 -0600

    First commit w/code - do challenges

--- 2D Array Challenges/src/TDArrayChallenges.java
+++ 2D Array Challenges/src/TDArrayChallenges.java
@@ -2,10 +2,38 @@
 public class TDArrayChallenges
 	{
 
+		static int[][] myArray = 
+					{
+						{3,5,9,2,-1,8,13},
+						{-2,5,1,11,8,2,0},
+						{9,1,-3,4,8,4,8},
+						{6,0,0,-1,4,12,7},
+						{2,6,-1,5,3,10,0},
+						{2,5,1,7,9,2,3},
+						{-3,1,1,4,11,12,4}
+								
+					};
+		
 		public static void main(String[] args)
 			{
-				
-
+				challenge1();
+				challenge2();
+				challenge3();
 			}
+		
+		public static void challenge1()
+		{
+			
+		}
+		
+		public static void challenge2()
+		{
+			
+		}
+		
+		public static void challenge3()
+		{
+			
+		}
 
 	}
