34bbc8587f815da9fd6a3bd01158164ebb0338d3
local_admin <local_admin@10.22.22.116>
2019-09-11 07:54:17 -0600

    Second and last commit - finished

--- 2D Array Challenges/src/TDArrayChallenges.java
+++ 2D Array Challenges/src/TDArrayChallenges.java
@@ -1,16 +1,17 @@
+import java.lang.reflect.Array;
 
 public class TDArrayChallenges
 	{
 
 		static int[][] myArray = 
 					{
-						{3,5,9,2,-1,8,13},
-						{-2,5,1,11,8,2,0},
-						{9,1,-3,4,8,4,8},
-						{6,0,0,-1,4,12,7},
-						{2,6,-1,5,3,10,0},
+						{3,5,9,2,1,8,13},
+						{2,5,1,11,8,2,0},
+						{9,1,3,4,8,4,8},
+						{6,0,0,1,4,12,7},
+						{2,6,1,5,3,10,0},
 						{2,5,1,7,9,2,3},
-						{-3,1,1,4,11,12,4}
+						{3,1,1,4,11,12,4}
 								
 					};
 		
@@ -21,19 +22,66 @@ public class TDArrayChallenges
 				challenge3();
 			}
 		
+		//Printing the largest number in the array
 		public static void challenge1()
 		{
+			int maxValue = 0;
 			
+			System.out.println("The program will print the largest number in the array.");
+			
+			for(int row = 0; row < myArray.length; row++)
+			{
+				for(int col = 0; col < myArray[row].length; col++)
+				{
+					if(myArray [row][col] > maxValue)
+					{
+						maxValue = myArray[row][col];
+					}
+				}
+			}
+			
+			System.out.println("The maximum value in the array is " + maxValue);
 		}
 		
+		//Printing the smallest number in the array
 		public static void challenge2()
 		{
+			int minValue = 0;
 			
+			System.out.println("The program will print the smallest number in the array.");
+			
+			for(int row = 0; row > myArray.length; row++)
+			{
+				for(int col = 0; col > myArray[row].length; col++)
+				{
+					if(myArray [row][col] < minValue)
+					{
+						minValue = myArray[row][col];
+					}
+				}
+			}
+			
+			System.out.println("The minimum value in the array is " + minValue);
 		}
 		
+		//Printing the average of all the numbers in the array
 		public static void challenge3()
 		{
+			double total = 0;
+			int totalLength = 0;
 			
+			System.out.println("The program will calculate the average of all the numbers in the array.");
+			
+			for(int row = 0; row < myArray.length; row++)
+			{
+				for(int col = 0; col < myArray[row].length; col++)
+				{
+					total += myArray[row][col];
+					totalLength++;
+				}
+			}
+			
+			System.out.println("The average of all the numbers in the array " + total / totalLength);
 		}
 
 	}
